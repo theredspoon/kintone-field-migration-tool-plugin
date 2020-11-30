54e7c6a0405c2ddd6b4c6dd209ddfb17a428c7b4
Nim <n001017@cybozu.onmicrosoft.com>
2020-11-30 13:00:19 -0800

    Add config pseudocode

--- src/js/config.js
+++ src/js/config.js
@@ -3,4 +3,14 @@
 
   var config = kintone.plugin.app.getConfig(PLUGIN_ID);
   
+  // get field list
+
+  // populate source field dropdown with appropriate options from field list
+
+  // based on selection of source field dropdown, generate target field list
+  
+  // populate target field dropdown with options from new target field list
+
+  // click button to initiate migration
+
 })(kintone.$PLUGIN_ID);
